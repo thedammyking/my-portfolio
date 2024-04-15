17dcaf2de45376e46865131d3e0067237d03a9f7
Oluwadamilola Babalola <kingdammie@gmail.com>
2024-04-15 17:21:24 +0100

    update 404 page

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -19,11 +19,13 @@ interface ErrorPageProps {
 const ErrorPage: React.FC<ErrorPageProps> = ({ title, subTitle, description, action }) => {
   return (
     <main className='w-full h-full relative text-center flex flex-col justify-center items-center text-black dark:text-white pt-[88px] pb-[87px] px-6'>
-      <h1 className='text-[70px] md:text-[140px] mb-[16px] md:mb-[30px] leading-normal'>{title}</h1>
-      <h3 className='text-[28px] md:text-5xl mb-[8px] md:mb-[13px] leading-[33.60px] md:leading-[56px]'>
+      <h1 className='text-[70px] md:text-[140px] mb-[16px] md:mb-[30px] leading-normal font-bold'>
+        {title}
+      </h1>
+      <h3 className='text-[28px] md:text-5xl mb-[8px] md:mb-[13px] leading-[33.60px] font-medium md:leading-[56px]'>
         {subTitle}
       </h3>
-      <p className='text-opacity-80 text-xl mb-6 md:mb-10 leading-7'>{description}</p>
+      <p className='text-opacity-80 body-text text-lg mb-6 md:mb-10 leading-7'>{description}</p>
       <Button variant={ButtonVariant.Primary} onClick={action.onClick}>
         {action.label}
       </Button>
