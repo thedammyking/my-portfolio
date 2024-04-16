56a535971db649d922eeb1bff5991821782e35ef
Oluwadamilola Babalola <kingdammie@gmail.com>
2024-04-16 23:45:54 +0100

    fixed loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,8 +4,8 @@ import { Loader } from '@/assets';
 
 export default function Loading() {
   return (
-    <main className='w-full h-[78.3vh] relative'>
-      <Loader className='w-16 md:w-20 text-black dark:text-dark-grey-100 absolute top-[43%] left-[50%] translate-x-[-50%] translate-y-[-50%]' />
+    <main className='w-full h-[78.3vh] relative lg:mt-20'>
+      <Loader className='w-16 md:w-20 text-black dark:text-dark-grey-100 absolute top-[48%] left-[50%] translate-x-[-50%] translate-y-[-50%]' />
     </main>
   );
 }
